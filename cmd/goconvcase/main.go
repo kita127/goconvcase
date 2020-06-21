24b1529a73e53acc99ada143fc63fa3024b0a9c2
kita127 <kitada.kosuke@gmail.com>
2020-06-21 13:56:23 +0900

    update

--- cmd/goconvcase/main.go
+++ cmd/goconvcase/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kita127/goconvcase"
 )
 
 func main() {
 
-	fmt.Println(goconvcase.Hoge())
+	goconvcase.Sample()
 }
--- goconvcase.go
+++ goconvcase.go
@@ -1,5 +1,48 @@
 package goconvcase
 
-func Hoge() string {
-	return "hoge"
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+)
+
+func Sample() {
+	src := `package foo
+
+import (
+	"fmt"
+	"time"
+)
+
+// これは関数
+func bar() {
+
+    // 関数の中身
+
+	fmt.Println(time.Now())
+}`
+
+	fset := token.NewFileSet() // positions are relative to fset
+
+	// Parse src but stop after processing the imports.
+	node, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ast.Print(fset, node)
+
+	var buf bytes.Buffer
+	err = format.Node(&buf, fset, node)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(buf.String())
+
 }
