594bfe41acfc1963ef7eb8b532eb439692975bf5
kita127 <kitada.kosuke@gmail.com>
2020-06-26 21:24:59 +0900

    update

--- goconvcase.go
+++ goconvcase.go
@@ -131,6 +131,9 @@ func (c *LSnake) IsThisCase(name string) bool {
 		return false
 	}
 	ss := strings.Split(name, "_")
+	if ss[0] == "" || ss[len(ss)-1] == "" {
+		return false
+	}
 	if len(ss) > 1 {
 		s := strings.Join(ss, "")
 		for _, c := range []rune(s) {
